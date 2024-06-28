860b76e513593d7579838404a2caad2403a21f21
Ricardo <ricardogithub17@gmail.com>
2024-06-28 10:08:02 -0300

    com efeito de demorar pra carregar

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -4,3 +4,20 @@ btnMenu.addEventListener('click',function (){
     navMenu.classList.toggle('aparecer')
     btnMenu.classList.toggle('aparecer-botao')
 })  
+window.sr = ScrollReveal({ reset:true});
+
+sr.reveal('.inicio-img', {duration:1000});
+sr.reveal('.titulo-menor', {duration:2000});
+sr.reveal('.paragrafos', {duration:3000});
+sr.reveal('.subtitulo', {duration:2000});
+sr.reveal('.info', {duration:5000});
+sr.reveal('.planos-card', {duration:2000});
+sr.reveal('.planos-beneficios', {duration:2000});
+sr.reveal('.turbinar-img', {duration:1000});
+sr.reveal('.rodape', {duration:1000});
+sr.reveal('.copy', {duration:2000});
+
+sr.reveal('.card1', {duration: 2000});
+sr.reveal('.card2', {duration: 2000});
+sr.reveal('.card3', {duration: 2000});
+sr.reveal('.card4', {duration: 2000});
