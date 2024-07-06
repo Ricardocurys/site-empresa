098b6188bba4403d6188694beeaec173c145b1af
Ricardo <ricardogithub17@gmail.com>
2024-07-06 14:51:36 -0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,26 @@ btnMenu.addEventListener('click',function (){
     btnMenu.classList.toggle('aparecer-botao')
     navMenu.classList.remove('link-clicado')
 })
-window.sr = ScrollReveal({ reset:true});
+window.addEventListener('load', atrasarCarregamento)
 
-sr.reveal('.inicio-img', {duration:500});
-sr.reveal('.titulo-menor', {duration:1000});
-sr.reveal('.paragrafos', {duration:2000});
-sr.reveal('.subtitulo', {duration:1000});
-sr.reveal('.info', {duration:4000});
-sr.reveal('.planos-card', {duration:1000});
-sr.reveal('.planos-beneficios', {duration:1000});
-sr.reveal('.turbinar-img', {duration:500});
-sr.reveal('.rodape', {duration:500});
-sr.reveal('.copy', {duration:1000});
-sr.reveal('.card', {duration: 1000});
+function atrasarCarregamento(){
+
+    window.sr = ScrollReveal({ reset:true})
+    sr.reveal('.inicio-img', {duration:500});
+    sr.reveal('.titulo-menor', {duration:1000});
+    sr.reveal('.paragrafos', {duration:2000});
+    sr.reveal('.subtitulo', {duration:1000});
+    sr.reveal('.info', {duration:4000});
+    sr.reveal('.planos-card', {duration:1000});
+    sr.reveal('.planos-beneficios', {duration:1000});
+    sr.reveal('.turbinar-img', {duration:500});
+    sr.reveal('.rodape', {duration:500});
+    sr.reveal('.copy', {duration:1000});
+    sr.reveal('.card', {duration: 1000});
+
+}
+
+
 
 
 const menuItems = document.querySelectorAll('.menu-nav-lista-itens');
